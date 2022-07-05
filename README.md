27be9a53732a187b40b38d864cabc5309c075235
Shaun Osborne <Shaun@Shauns-MBPro.local>
2022-07-05 17:33:51 +0930

    first v2 release (bugs+shortcode processor)

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # PHP Siteframework
 This is the 2022 version of [this old gem](https://github.com/Cybergate9/phpSiteFramework)..
-The update is to become version 2.
+This update becomes version 2.
 
 ## Version 2
 
 Version 2 adds:
 
-* ability to deliver markdown content including use of front matter (requires Parsedown)
+* ability to deliver markdown content including use of front matter (requires Parsedown and Parsedown Extra)
 
 * Cache-Lite replaced by internal pure php caching
+
+* {{shortcode}} processor added to supplement <!-- SF_command.* --> functionality (adds Venodox lightbox js)
 
 * `_SF_modules/admin/index.html` shows config sanity checks
 
--- _SF_modules/SF_mainconfig.php
+++ _SF_modules/SF_mainconfig.php
@@ -3,62 +3,60 @@
 * This file is Siteframework's main configuration file
 *
 *
-* @package PHP-SiteFramework
 * @author Shaun Osborne (webmaster@cybergate9.net)
+*
 * @link https://github.com/Cybergate9/PHP-Siteframework
-* @access public 
+*
 * @copyright Shaun Osborne, 2005-present
 * @license https://github.com/Cybergate9/PHP-Siteframework/blob/master/LICENSE
 */
-        
 
 /****************************************************************************
 Global derived configuration values - shouldn't need to change these */
-$SF_modulesdirname='_SF_modules/';
-$SF_moduleswebpath=$SF_sitewebpath.$SF_modulesdirname;
-$SF_modulesdrivepath=$SF_documentroot.$SF_moduleswebpath;
-$SF_sitedrivepath=$SF_documentroot.$SF_sitewebpath;
-$SF_subsitewebpath=$SF_sitewebpath;
-$SF_subsitedrivepath=$SF_sitedrivepath;
-$SF_phpselfdrivepath=$SF_documentroot.$_SERVER['PHP_SELF'];
-$SF_sitelogo=$SF_moduleswebpath.'images/sflogo_sml.jpg';
-
+$SF_modulesdirname = '_SF_modules/';
+$SF_moduleswebpath = $SF_sitewebpath.$SF_modulesdirname;
+$SF_modulesdrivepath = $SF_documentroot.$SF_moduleswebpath;
+$SF_sitedrivepath = $SF_documentroot.$SF_sitewebpath;
+$SF_subsitewebpath = $SF_sitewebpath;
+$SF_subsitedrivepath = $SF_sitedrivepath;
+$SF_phpselfdrivepath = $SF_documentroot.$_SERVER['PHP_SELF'];
+$SF_sitelogo = $SF_moduleswebpath.'images/sflogo_sml.jpg';
 
 /****************************************************************************
 Global default values - shouldn't need to change these */
 
 /* include parsedown and extra paths*/
-$SF_parsedownpath=$SF_modulesdrivepath.'vendor/erusev/parsedown/Parsedown.php';
-$SF_parsedownextrapath=$SF_modulesdrivepath.'vendor/erusev/parsedown-extra/ParsedownExtra.php';
+$SF_parsedownpath = $SF_modulesdrivepath.'vendor/erusev/parsedown/Parsedown.php';
+$SF_parsedownextrapath = $SF_modulesdrivepath.'vendor/erusev/parsedown-extra/ParsedownExtra.php';
 
 /* data files */
-$defaultmenudatafile=$SF_modulesdrivepath.'SF_default_config_menu.csv';
-$defaultdirconfigfile=$SF_modulesdrivepath.'SF_default_config_dir.csv';
-$defaultsiteconfigfile=$SF_modulesdrivepath.'SF_default_config_site.csv';
+$defaultmenudatafile = $SF_modulesdrivepath.'SF_default_config_menu.csv';
+$defaultdirconfigfile = $SF_modulesdrivepath.'SF_default_config_dir.csv';
+$defaultsiteconfigfile = $SF_modulesdrivepath.'SF_default_config_site.csv';
 
 /* site default header, footer and css */
-$defaultheaderfile=$SF_modulesdrivepath.'SF_defaultheader.html';
-$defaultfooterfile=$SF_modulesdrivepath.'SF_defaultfooter.html';
-$defaultcssfile=$SF_moduleswebpath.'SF_default.css';
+$defaultheaderfile = $SF_modulesdrivepath.'SF_defaultheader.html';
+$defaultfooterfile = $SF_modulesdrivepath.'SF_defaultfooter.html';
+$defaultcssfile = $SF_moduleswebpath.'SF_default.css';
 
 /*metadata files if used*/
-$defaultmetadatafile=$SF_modulesdrivepath."SF_defaultmetadata.html";
+$defaultmetadatafile = $SF_modulesdrivepath.'SF_defaultmetadata.html';
 
 /* print view files */
-$defaultprintheaderfile=$SF_modulesdrivepath.'SF_defaultprintheader.html';
-$defaultprintfooterfile=$SF_modulesdrivepath.'SF_defaultprintfooter.html';
-$defaultprintcssfile=$SF_moduleswebpath.'SF_defaultprint.css';
+$defaultprintheaderfile = $SF_modulesdrivepath.'SF_defaultprintheader.html';
+$defaultprintfooterfile = $SF_modulesdrivepath.'SF_defaultprintfooter.html';
+$defaultprintcssfile = $SF_moduleswebpath.'SF_defaultprint.css';
 
 /* text only view files */
-$defaulttextonlycssfile=$SF_moduleswebpath.'SF_textonly.css';
+$defaulttextonlycssfile = $SF_moduleswebpath.'SF_textonly.css';
 
-$menutoplevelidentifier='_toplevel';
+$menutoplevelidentifier = '_toplevel';
 
 /* just in case we've come directly here (not via autoprepend) then set sfdebug to 0 if it's not set already */
-if(!isset($sfdebug)){$sfdebug = 0;}
+if (! isset($sfdebug)) {
+    $sfdebug = 0;
+}
 
-if($sfdebug >= 3)
-  {
+if ($sfdebug >= 3) {
     SF_DebugMsg($SF_modulesdrivepath.'SF_mainconfig.php loaded');
-  }
-?>
+}
